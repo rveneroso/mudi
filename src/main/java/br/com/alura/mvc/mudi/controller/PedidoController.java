811817e41b82a23e00b39039278710e9d0a8a3f2
Renato Veneroso <rveneroso@gmail.com>
2022-01-15 19:16:13 -0300

    Alterações diversas de laytout e inclusão de algumas anotações

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -2,6 +2,8 @@ package br.com.alura.mvc.mudi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +20,18 @@ public class PedidoController {
 	private PedidoRepository pedidoRepository;
 
 	@GetMapping("formulario")
-	public String formulario() {
+	public String formulario(RequisicaoNovoPedido requisicaoNovoPedido) {
 		return "pedido/formulario";
 	}
 	
 	@PostMapping("novo")
-	public String novo(RequisicaoNovoPedido requisicao) {
-		Pedido pedido = requisicao.toPedido();
+	public String novo(@Validated RequisicaoNovoPedido requisicaoNovoPedido, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "/pedido/formulario";
+		}
+		
+		Pedido pedido = requisicaoNovoPedido.toPedido();
 		pedidoRepository.save(pedido);
 		return "/pedido/formulario";
 	}
